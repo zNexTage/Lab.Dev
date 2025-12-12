f1c14e55e65f977284fedcbcb2fb22f08aca395d
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-12-11 21:05:58 -0300

    Criado modelo Aluno.

---
+++ ASP.NET Core Expert/ASP.NET MVC/MVC.FormLab/Models/Aluno.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC.FormLab.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres")]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [DataType(DataType.Date, ErrorMessage = "O campo {0} está em formato incorreto")]
+        [Display(Name = "Nome")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
+        [RegularExpression(@"/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$/", ErrorMessage = "O campo {0} está em formato inválido")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(60, ErrorMessage = "O campo {0} precisa ter no máximo {1} caracteres")]
+        [Compare("Email", ErrorMessage ="Os emails informados não são identicos")]
+        [Display(Name = "Confirme o email")]
+        [NotMapped]
+        public string? EmailConfirmacao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
