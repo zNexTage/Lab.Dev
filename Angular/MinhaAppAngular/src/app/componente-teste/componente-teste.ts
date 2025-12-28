e49295b483289bde0b6db71abcdffa643b09c896
Christian Oliveira <christianoliveira1908@hotmail.com>
2025-12-28 14:23:36 -0300

    Módulos e decorators

---
+++ Angular/MinhaAppAngular/src/app/componente-teste/componente-teste.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-teste',
+  imports: [],
+  templateUrl: './componente-teste.html',
+  styleUrl: './componente-teste.css',
+})
+export class ComponenteTeste implements OnInit {
+  ngOnInit(): void {
+    
+  }
+  
+}
---
+++ Angular/MinhaAppAngular/src/app/funcionalidade/funcionalidade-module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // Seus componentes
+  ],
+  imports: [
+    // Seus módulos
+    CommonModule
+  ],
+  providers: [
+    // seus serviços
+  ]
+})
+export class FuncionalidadeModule { }
