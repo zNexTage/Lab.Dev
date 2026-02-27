f55ab326f8df40531ddf1775658999b00ae4b540
Christian Oliveira <christianoliveira1908@hotmail.com>
2026-02-27 10:23:15 -0300

    Algoritmo: Trie

---
+++ Ferramentas algoritmos e outras coisas/Lab.DesenvTools/Lab.DesenvTools.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Lab.DesenvTools.Console;
+
+var trie = new TrieNode();
+
+List<List<string>> queries =
+[
+    ["add", "hack"],
+    ["add", "hackerrank"],
+    ["find", "hac"],
+    ["find", "hak"],
+];
+
+var result = new List<int>();
+
+queries.ForEach(q => {
+    var command = q[0];
+    var item = q[1];
+
+    if (command == "add")
+    {
+        trie.Insert(item);
+    }
+    else if (command == "find")
+    {
+        var node = trie.GetNode(item);
+
+        if (node is null)
+        {
+            result.Add(0);
+            return;
+        }
+
+        result.Add(node.Count);
+    }
+});
+
+Console.WriteLine($"[{string.Join(", ", result)}]");
+Console.ReadLine();
---
+++ Ferramentas algoritmos e outras coisas/Lab.DesenvTools/Lab.DesenvTools.Console/TrieNode.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab.DesenvTools.Console
+{
+    public class Node
+    {
+        public Node()
+        {
+            Children = new Dictionary<char, Node>();
+            Count = 0;
+        }
+
+        public Dictionary<char, Node> Children
+        {
+            get;
+            set;
+        }
+
+        public bool IsCompleteWord
+        {
+            get;
+            set;
+        }
+
+        public int Count
+        {
+            get;
+            set;
+        }
+    }
+
+
+    public class TrieNode
+    {
+        public Node Root { get; private set; }
+
+        public TrieNode()
+        {
+            Root = new Node();
+        }
+
+        public void Insert(string word)
+        {
+            Node current = Root;
+
+            char[] characters = word.ToCharArray();
+
+            foreach (var c in characters)
+            {
+                Node child = current.Children.GetValueOrDefault(c);
+
+                if (child is null)
+                {
+                    child = new Node();
+                    child.Count += 1;
+                    current.Children.Add(c, child);
+                } else
+                {
+                    child.Count += 1;
+                }
+
+                current = child;
+            }
+
+            current.IsCompleteWord = true;
+        }
+
+        public Node GetNode(string word)
+        {
+            Node current = Root;
+
+            char[] characteres = word.ToCharArray();
+
+            foreach (var c in characteres)
+            {
+                Node child = current.Children.GetValueOrDefault(c);
+
+                if (child is null) return null;
+                current = child;
+            }
+
+            return current;
+        }
+    }
+}
